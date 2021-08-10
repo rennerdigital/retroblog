0ec2c9fbbf601da460fb2baaa7cf87b14715bbb6
Renner <rennermccreath@gmail.com>
2021-08-10 18:58:57 -0400

    fixed some scaffolding

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var createError = require('http-errors');
+var path = require('path');
+var logger = require('morgan');
 const express = require('express')
 const mongoose = require('mongoose')
 const Post = require('./models/post')
@@ -7,9 +10,13 @@ const app = express()
 
 mongoose.connect('mongodb://localhost/retroblog', { useNewUrlParser: true, useUnifiedTopology: true })
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
-app.use(express.urlencoded({ extended: false }))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
@@ -18,5 +25,23 @@ app.get('/', async (req, res) => {
 })
 
 app.use('/posts', postRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
+  module.exports = app;
 
 app.listen(3000)
